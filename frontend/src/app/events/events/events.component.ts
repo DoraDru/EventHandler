84c87f2bb55e2878e226b9c1cccc74db686a32d8
Dori Balogh <132360287+DoraDru@users.noreply.github.com>
2024-06-14 19:43:14 +0200

    frontend - add event component to display the fetched eves list

---
+++ frontend/src/app/events/events/events.component.ts
@@ -0,0 +1,24 @@
+import { NgFor } from '@angular/common';
+import { Component } from '@angular/core';
+import { EventModel } from '../event.model';
+import { EventService } from '../event.service';
+
+@Component({
+  selector: 'app-events',
+  standalone: true,
+  imports: [NgFor],
+  providers: [EventService],
+  templateUrl: './events.component.html',
+  styleUrl: './events.component.css'
+})
+export class EventsComponent {
+  events: EventModel[] = [];
+
+  constructor(private service: EventService) {}
+
+  ngOnInit(): void {
+    this.service.getEvents().subscribe((data) => {
+      this.events = data;
+    });
+  }
+}
