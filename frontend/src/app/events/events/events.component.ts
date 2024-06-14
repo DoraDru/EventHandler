955ce2bcdf140b73b7471d04452a135cf9aa3742
Dori Balogh <132360287+DoraDru@users.noreply.github.com>
2024-06-14 19:58:59 +0200

    create pipe to shorten event details

--- frontend/src/app/events/events/events.component.ts
+++ frontend/src/app/events/events/events.component.ts
@@ -2,11 +2,12 @@ import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 import { EventModel } from '../event.model';
 import { EventService } from '../event.service';
+import { ShortenTextPipe } from '../pipes/shorten.pipe';
 
 @Component({
   selector: 'app-events',
   standalone: true,
-  imports: [NgFor],
+  imports: [NgFor, ShortenTextPipe],
   providers: [EventService],
   templateUrl: './events.component.html',
   styleUrl: './events.component.css'
---
+++ frontend/src/app/events/pipes/shorten.pipe.ts
@@ -0,0 +1,14 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'shorten',
+  standalone: true,
+})
+export class ShortenTextPipe implements PipeTransform {
+  transform(value: String, limit: number) {
+    if (value.length > limit) {
+      return value.substring(0, limit) + '...';
+    }
+    return value;
+  }
+}
