6c0f481e557d210133ad5e6b28bd5b362741c27f
Dori Balogh <132360287+DoraDru@users.noreply.github.com>
2024-06-14 19:57:28 +0200

    frontend - add EventComponent to AppComponent

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { NgFor, NgIf } from '@angular/common';
+import { EventService } from './events/event.service';
+import { HeaderComponent } from './header/header.component';
+import { EventsComponent } from './events/events/events.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    HttpClientModule,
+    NgIf,
+    NgFor,
+    HeaderComponent,
+    EventsComponent,
+  ],
+  providers: [EventService],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-
-}
+export class AppComponent {}
