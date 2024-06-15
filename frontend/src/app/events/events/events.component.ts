15de39fbd37cb03a8a7c057e6fc2dfc078786b2d
Dori Balogh <132360287+DoraDru@users.noreply.github.com>
2024-06-15 19:18:53 +0200

    frontend - add routerLink to event details end header links

--- frontend/src/app/events/events/events.component.ts
+++ frontend/src/app/events/events/events.component.ts
@@ -3,11 +3,12 @@ import { Component } from '@angular/core';
 import { EventModel } from '../event.model';
 import { EventService } from '../event.service';
 import { ShortenTextPipe } from '../pipes/shorten.pipe';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-events',
   standalone: true,
-  imports: [NgFor, ShortenTextPipe],
+  imports: [NgFor, ShortenTextPipe, RouterLink],
   providers: [EventService],
   templateUrl: './events.component.html',
   styleUrl: './events.component.css'
