c097fdbb75c970c8e341745d7c781b9b5a5a396c
Dori Balogh <132360287+DoraDru@users.noreply.github.com>
2024-06-15 21:05:44 +0200

    frontend - bugfix - correct paths and routes sequence

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,12 +2,13 @@ import { Routes } from '@angular/router';
 import { EventsComponent } from './events/events/events.component';
 import { EventDetailComponent } from './events/event-detail/event-detail.component';
 import { HomeComponent } from './home/home.component';
+import { EditEventComponent } from './events/edit-event/edit-event.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'events', component: EventsComponent },
+  { path: 'events/new', component: EditEventComponent },
   { path: 'events/:id', component: EventDetailComponent },
-  { path: 'new', component: EventDetailComponent },
-  { path: ':id/edit', component: EventDetailComponent },
+  { path: 'events/:id/edit', component: EventDetailComponent },
   { path: '**', redirectTo: '' },
 ];
