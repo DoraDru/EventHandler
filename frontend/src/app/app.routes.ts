b57e7fbc35ea7957fd1bd9ec130a64384eee4e79
Dori Balogh <132360287+DoraDru@users.noreply.github.com>
2024-06-16 19:30:55 +0200

    frontend - create not found page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { EventsComponent } from './events/events/events.component';
 import { EventDetailComponent } from './events/event-detail/event-detail.component';
 import { HomeComponent } from './home/home.component';
 import { EditEventComponent } from './events/edit-event/edit-event.component';
+import { ErrorPageComponent } from './error-page/error-page.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -10,5 +11,6 @@ export const routes: Routes = [
   { path: 'events/new', component: EditEventComponent },
   { path: 'events/:id', component: EventDetailComponent },
   { path: 'events/:id/edit', component: EditEventComponent },
+  { path: 'error-page', component: ErrorPageComponent },
   { path: '**', redirectTo: '' },
 ];
