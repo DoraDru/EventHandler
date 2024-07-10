7300c06b9191146f8f4ab441f792af36ccb6eee3
Dori Balogh <132360287+DoraDru@users.noreply.github.com>
2024-07-10 20:00:40 +0200

    backend - create UserService - user can register

---
+++ backend/src/main/java/com/codecool/backend/service/UserService.java
@@ -0,0 +1,40 @@
+package com.codecool.backend.service;
+
+import com.codecool.backend.model.Role;
+import com.codecool.backend.model.UserEntity;
+import com.codecool.backend.model.dto.UserDTO;
+import com.codecool.backend.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public boolean register(UserDTO user) {
+        if (userRepository.getUserEntityByName(user.getName()).isEmpty()) {
+            UserEntity newUser = new UserEntity();
+            newUser.setName(user.getName());
+            newUser.setPassword(user.getPassword());
+            newUser.add(Role.ROLE_USER);
+            this.userRepository.save(newUser);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void login() {
+
+    }
+
+    private UserEntity getUserByName(String name) {
+        Optional<UserEntity> user = this.userRepository.getUserEntityByName(name);
+        return user.orElseThrow(() -> new IllegalArgumentException(String.format("User not found: %s", name)));
+    }
+}
