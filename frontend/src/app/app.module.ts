7f6e2263236676df0883d003daf41627cda0a772
Dori Balogh <132360287+DoraDru@users.noreply.github.com>
2024-07-13 19:04:11 +0200

    frontend - add auth interceptor

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,19 +1,20 @@
-import { NgModule } from "@angular/core";
-import { AppComponent } from "./app.component";
-import { HeaderComponent } from "./header/header.component";
-import { EventsComponent } from "./events/events/events.component";
-import { HomeComponent } from "./home/home.component";
-import { EventDetailComponent } from "./events/event-detail/event-detail.component";
-import { EditEventComponent } from "./events/edit-event/edit-event.component";
-import { EventService } from "./events/event.service";
-import { BrowserModule } from "@angular/platform-browser";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { HttpClientModule } from "@angular/common/http";
-import { AppRoutingModule } from "./app.routes";
-import { ShortenTextPipe } from "./events/pipes/shorten.pipe";
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { EventsComponent } from './events/events/events.component';
+import { HomeComponent } from './home/home.component';
+import { EventDetailComponent } from './events/event-detail/event-detail.component';
+import { EditEventComponent } from './events/edit-event/edit-event.component';
+import { EventService } from './events/event.service';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app.routes';
+import { ShortenTextPipe } from './events/pipes/shorten.pipe';
 import { LoginComponent } from './user/login/login.component';
 import { RegistrationComponent } from './user/registration/registration.component';
-import { UserService } from "./user/user.service";
+import { UserService } from './user/user.service';
+import { AuthInterceptorService } from './auth/auth-interceptor.service';
 
 @NgModule({
   declarations: [
@@ -25,19 +26,24 @@ import { UserService } from "./user/user.service";
     EditEventComponent,
     ShortenTextPipe,
     LoginComponent,
-    RegistrationComponent
+    RegistrationComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
-  providers: [EventService, UserService],
+  providers: [
+    EventService,
+    UserService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptorService,
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent],
-
 })
-export class AppModule{
-
-}
+export class AppModule {}
---
+++ frontend/src/app/auth/auth-interceptor.service.ts
@@ -0,0 +1,33 @@
+import {
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpParams,
+  HttpRequest,
+} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { UserService } from '../user/user.service';
+import { exhaustMap, Observable, take } from 'rxjs';
+
+@Injectable()
+export class AuthInterceptorService implements HttpInterceptor {
+  constructor(private userService: UserService) {}
+
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    return this.userService.user.pipe(
+      exhaustMap((user) => {
+        if (user && user.jwt) {
+          req = req.clone({
+            setHeaders: {
+              Authorization: `Bearer ${user.jwt}`,
+            },
+          });
+        }
+        return next.handle(req);
+      })
+    );
+  }
+}
