a1b4c621bc63deeb724d3f45b2828640f91d9ee2
Dori Balogh <132360287+DoraDru@users.noreply.github.com>
2024-07-13 17:53:46 +0200

    frontend - welcome message based on user

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { UserService } from '../user/user.service';
+import { Subscription } from 'rxjs';
+import { AuthenticatedUser } from '../auth/authenticated-user.model';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, OnDestroy {
+  userSub!: Subscription;
+  currentUser: AuthenticatedUser | null = null;
 
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.userSub = this.userService.user.subscribe((user) => {
+      this.currentUser = user;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.userSub.unsubscribe();
+  }
 }
